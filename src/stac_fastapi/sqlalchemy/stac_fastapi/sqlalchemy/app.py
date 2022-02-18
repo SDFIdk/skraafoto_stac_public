9757af27037251c39166b7b8bbbf72d7dc83246d
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2022-02-18 14:07:51 +0100

    Outcommented code to check if is should be removed?

--- src/stac_fastapi/sqlalchemy/stac_fastapi/sqlalchemy/app.py
+++ src/stac_fastapi/sqlalchemy/stac_fastapi/sqlalchemy/app.py
@@ -80,35 +80,35 @@ if settings.debug:
         return response
 
 
-def run():
-    """Run app from command line using uvicorn if available."""
-    try:
-        import uvicorn
+# def run():
+#     """Run app from command line using uvicorn if available."""
+#     try:
+#         import uvicorn
 
-        uvicorn.run(
-            "stac_fastapi.sqlalchemy.app:app",
-            host=settings.app_host,
-            port=settings.app_port,
-            log_level="info",
-            reload=settings.reload,
-        )
-    except ImportError:
-        raise RuntimeError("Uvicorn must be installed in order to use command")
-
-
-if __name__ == "__main__":
-    run()
+#         uvicorn.run(
+#             "stac_fastapi.sqlalchemy.app:app",
+#             host=settings.app_host,
+#             port=settings.app_port,
+#             log_level="info",
+#             reload=settings.reload,
+#         )
+#     except ImportError:
+#         raise RuntimeError("Uvicorn must be installed in order to use command")
 
 
-def create_handler(app):
-    """Create a handler to use with AWS Lambda if mangum available."""
-    try:
-        # from mangum import Mangum
-
-        # return Mangum(app)
-        pass
-    except ImportError:
-        return None
+# if __name__ == "__main__":
+#     run()
 
 
-handler = create_handler(app)
+# def create_handler(app):
+#     """Create a handler to use with AWS Lambda if mangum available."""
+#     try:
+#         # from mangum import Mangum
+
+#         # return Mangum(app)
+#         pass
+#     except ImportError:
+#         return None
+
+
+# handler = create_handler(app)
